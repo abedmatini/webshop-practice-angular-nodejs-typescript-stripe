51f5d89b2b8a5d59b1310a54279dd3486b6cad78
Abed.Matini <abed.matini@ogilvy.co.za>
2023-06-12 23:54:28 +0200

    first commit

---
+++ store/src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: 'filters.component.html'
+})
+export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
+  categories = ['shoes', 'sports'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  }
+
+}
